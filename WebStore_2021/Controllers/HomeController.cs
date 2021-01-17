ab29181bea1cbd0f5517a0d0a36767ba2a5b756d
K2INcHO <k3incho@inbox.ru>
2021-01-17 18:26:36 +0300

    Представления

---
+++ WebStore_2021/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore_2021.Controllers
+{
+    //контроллер нужен для того, чтобы обработать входящее подключение
+    //на каждый контроллер должно быть свое представление (View)
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("SecondView");
+
+        public IActionResult SecondAction()
+        {
+            return Content("Second controller action");
+        }
+    }
+}
