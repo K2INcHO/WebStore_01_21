6be3fe612cb0ba28176435c482c6ff255f86a578
K2INcHO <k3incho@inbox.ru>
2021-01-17 18:52:56 +0300

    Визуализация списка сотрудников

--- WebStore_2021/Controllers/HomeController.cs
+++ WebStore_2021/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore_2021.Models;
 
 namespace WebStore_2021.Controllers
 {
@@ -6,11 +8,24 @@ namespace WebStore_2021.Controllers
     //на каждый контроллер должно быть свое представление (View)
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 37 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 25 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 27 },
+        };
+
         public IActionResult Index() => View("SecondView");
 
         public IActionResult SecondAction()
         {
             return Content("Second controller action");
+        }
+
+        //визуализируем список сотрудников
+        public IActionResult Employees()
+        {
+            return View(__Employees);
         }
     }
 }
