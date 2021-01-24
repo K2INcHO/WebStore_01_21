2d7ca32ffbe62df73c3f173ccd19e638e6ce3b85
K2INcHO <k3incho@inbox.ru>
2021-01-24 17:22:52 +0300

    Промежуточное ПО

--- WebStore_2021/Startup.cs
+++ WebStore_2021/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore_2021.Infrastructure.Middleware;
 
 namespace WebStore_2021
 {
@@ -13,17 +14,27 @@ namespace WebStore_2021
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
-
+        // Все вызовы к классу App - добавление промежуточного ПО (настройка конвейера)
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); // добавляется страница обработки исключений
             }
 
-            app.UseStaticFiles(); //подключаем статическое содержимое (по умолчанию будет жить в wwwroot)
+            app.UseStaticFiles(); //подключаем статическое содержимое (по умолчанию будет жить в wwwroot) - срабатывает проверка на обработку файла
 
-            app.UseRouting();
+            app.UseRouting(); // происходит извлечение информации о маршрутах
+
+            app.UseWelcomePage("/welcome");
+
+            app.UseMiddleware<TestMiddleware>();
+
+            app.MapWhen(
+                context => context.Request.Query.ContainsKey("id") && context.Request.Query["id"] == "5",
+                context => context.Run(async request => await request.Response.WriteAsync("Hello with id == 5!"))
+                ); // альтернатива Map
+            app.Map("/hello", context => context.Run(async request => await request.Response.WriteAsync("Hello!!!")));
 
             //прописываем маршруты
             app.UseEndpoints(endpoints =>
