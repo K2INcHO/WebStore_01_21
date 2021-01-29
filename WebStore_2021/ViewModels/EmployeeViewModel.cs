b8831764e96c0451a1e3924c7e412de9ad5616e1
K2INcHO <k3incho@inbox.ru>
2021-01-29 09:39:34 +0300

    Валидация данных форм

--- WebStore_2021/ViewModels/EmployeeViewModel.cs
+++ WebStore_2021/ViewModels/EmployeeViewModel.cs
@@ -1,20 +1,40 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebStore_2021.ViewModels
 {
-    public class EmployeeViewModel
+    public class EmployeeViewModel : IValidatableObject
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; init; }
 
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage ="Фамилия обязательна")]
+        [StringLength(15, MinimumLength =2, ErrorMessage = "Длина фамилии должна быть от 2 до 15 символов")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)" , ErrorMessage = "Неверный формат фамилии")]
         public string LastName { get; init; }
 
-        public string FirstName { get; init; }
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Имя обязательно")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 15 символов")]
+        public string Name { get; init; }
 
+        [Display(Name = "Отчество")]
         public string MiddleName { get; init; }
 
+        [Display(Name = "Возраст")]
+        [Range(18,80, ErrorMessage ="Сотрудник должен быть в возрасте от 18 до 80 лет")]
         public int Age { get; init; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext context)
+        {
+            //context.GetService();
+            yield return ValidationResult.Success;
+            yield return new ValidationResult("Текст ошибки", new[] { nameof(Name)});
+        }
     }
 }
