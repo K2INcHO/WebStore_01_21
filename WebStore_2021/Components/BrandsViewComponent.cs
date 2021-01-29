5a4d79c92072b09eb9bd880237617455b76aad52
K2INcHO <k3incho@inbox.ru>
2021-01-29 12:24:01 +0300

    Компонент брендов

---
+++ WebStore_2021/Components/BrandsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore_2021.Infrastructure.Interfaces;
+using WebStore_2021.ViewModels;
+
+namespace WebStore_2021.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+        public IViewComponentResult Invoke() => View(GetBrands());
+
+        private IEnumerable<BrandsViewModel> GetBrands() =>
+            _ProductData.GetBrands()
+                .OrderBy(brand => brand.Order)
+                .Select(brand => new BrandsViewModel
+                {
+                    Id = brand.Id,
+                    Name = brand.Name
+                });
+    }
+}
---
+++ WebStore_2021/ViewModels/BrandsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebStore_2021.ViewModels
+{
+    public record BrandsViewModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+    }
+}
