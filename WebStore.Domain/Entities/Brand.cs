0be1065ba61217c31877dcc6f5ac25ae3491da6e
K2INcHO <k3incho@inbox.ru>
2021-01-29 10:12:56 +0300

    Описание сущностей Бренда и Секции

---
+++ WebStore.Domain/Entities/Brand.cs
@@ -0,0 +1,17 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    public class Brand : NamedEntity
+    {
+        public int Order { get; set; }
+    }
+
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
