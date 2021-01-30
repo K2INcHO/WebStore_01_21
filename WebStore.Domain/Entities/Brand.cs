20128eba6e63c93b52d438f762e05f050b71f62a
K2INcHO <k3incho@inbox.ru>
2021-01-30 17:42:25 +0300

    Доопределение сущностей для использования их контексте БД EFCore

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,9 +1,15 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
 
 namespace WebStore.Domain.Entities
 {
+    //[Table("Brands")]
     public class Brand : NamedEntity
     {
+        //[Column("BrandOrder")]
         public int Order { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }
