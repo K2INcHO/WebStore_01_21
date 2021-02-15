de4ee0701a3a3c0600e6369cf06da714721f41c1
K2INcHO <k3incho@inbox.ru>
2021-02-15 20:00:07 +0300

    Отображение списка товаров в области администратора

--- WebStore_2021/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore_2021/Areas/Admin/Controllers/ProductsController.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Domain.Entities.Identity;
+using WebStore_2021.Infrastructure.Interfaces;
 
 namespace WebStore_2021.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class ProductsController : Controller
     {
-        public IActionResult Index => View();
+        private readonly IProductData _ProductData;
+
+        public ProductsController(IProductData productData) => _ProductData = productData;
+
+        public IActionResult Index => View(_ProductData.GetProducts());
     }
 }
--- WebStore_2021/Infrastructure/Services/InSQL/SQLProductData.cs
+++ WebStore_2021/Infrastructure/Services/InSQL/SQLProductData.cs
@@ -22,7 +22,9 @@ namespace WebStore_2021.Infrastructure.Services.InSQL
 
         public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
         {
-            IQueryable<Product> query = _db.Products;
+            IQueryable<Product> query = _db.Products
+                .Include(p => p.Brand)
+                .Include(p => p.Section);
 
             if(Filter?.Ids?.Length > 0)
                 query = query.Where(product => Filter.Ids.Contains(product.Id));
